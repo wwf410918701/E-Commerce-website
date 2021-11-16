a63edf4df078d67d5db8e5da8087a22d00c6c760
WeiFeng Wu <wwf410918701@126.com>
2021-11-16 12:53:39 +0800

    add homepage and its corresponding components like diretory list and menu-items

---
+++ ecommerce-website/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import HomePage from './pages/homepage/homepage.component'
+
+function App() {
+  return (
+    <div><HomePage/></div>
+    
+  );
+}
+
+export default App;
---
+++ ecommerce-website/src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Diretory from "../../components/diretory/diretory.component"
+import './homepage.styles.scss'
+
+const HomePage = () => {
+    return(
+        <div className='homePage'>
+            <Diretory/>
+        </div>
+    )
+}
+
+export default HomePage
