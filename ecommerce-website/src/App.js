1edafe8e79305bcd4082877f0ff19a3b5ac37cc3
WeiFeng Wu <wwf410918701@126.com>
2021-11-17 18:07:44 +0800

    add header, signin page and relevant components

--- ecommerce-website/src/App.js
+++ ecommerce-website/src/App.js
@@ -4,6 +4,7 @@ import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
 import {Route,Switch} from 'react-router-dom'
 import Header from './components/header/header.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 
 
@@ -13,6 +14,7 @@ function App() {
       <Header/>
       <Switch>
         <Route exact path='/shop' component={ShopPage}/>
+        <Route exact path='/signin' component={SignInAndSignUp}/>
         <Route exact path='/' component={HomePage}/>
       </Switch>
     </div>
---
+++ ecommerce-website/src/components/form-input/form-input.component.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import './form-input.styles.scss'
+
+const FormInput = ({ handleChange, label, ...otherProperties}) => (
+    <div className='group'>
+        <input className='form-input' onChange={handleChange} {...otherProperties} />
+        {
+            label ? (
+                <label 
+                    className={`${
+                        otherProperties.value.length ? 'shrink' : ''
+                    } form-input-label`}
+                >
+                    {label}
+                </label>
+            ):null
+        }
+    </div>
+)
+
+export default FormInput
