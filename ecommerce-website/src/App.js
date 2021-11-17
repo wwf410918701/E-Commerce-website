ac0c93b82134802ef07ee92ddb76449b443e83ce
WeiFeng Wu <wwf410918701@126.com>
2021-11-17 15:17:04 +0800

    add shop page, header, collection-item, collection-preview component

--- ecommerce-website/src/App.js
+++ ecommerce-website/src/App.js
@@ -1,10 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component'
+import {Route,Switch} from 'react-router-dom'
+import Header from './components/header/header.component';
+
+
 
 function App() {
   return (
-    <div><HomePage/></div>
+    <div>
+      <Header/>
+      <Switch>
+        <Route exact path='/shop' component={ShopPage}/>
+        <Route exact path='/' component={HomePage}/>
+      </Switch>
+    </div>
+    
     
   );
 }
--- ecommerce-website/src/components/diretory/diretory.component.jsx
+++ ecommerce-website/src/components/diretory/diretory.component.jsx
@@ -47,8 +47,8 @@ class Diretory extends React.Component {
         return (
             <div className='diretory-menu'>
                 {
-                    this.state.sections.map(({id, title, imageUrl, size}) => {
-                        return <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+                    this.state.sections.map(({id, ...otherSectionProps}) => {
+                        return <MenuItem key={id} {...otherSectionProps}/>
                     })
                 }
             </div>
