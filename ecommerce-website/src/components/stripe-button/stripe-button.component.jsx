47ac366d92710547f9fb563b9f73b1992cc2731a
WeiFeng Wu <wwf410918701@126.com>
2021-11-25 11:40:00 +0800

    integrate stripe to mimic payment, alter title and incon inthe tap

---
+++ ecommerce-website/src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100
+    const publishableKey = 'pk_test_51JzYDuKKAe08nYQuSmVnZOHv48xXJPpQzoM4QaUEgIsJB7No06LkJ0W9V4U7ObOEuJC1J2UDafyOQD8OL22R8M6C00gamtzpcg';
+
+    const onToken = token => {
+        alert('Payment success!');
+    }
+
+    return (
+        <StripeCheckout
+            label='Pay Now'
+            name='CRWN Clothing Ltd'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    )
+}
+
+export default StripeCheckoutButton;
--- ecommerce-website/src/pages/checkout/checkout.component.jsx
+++ ecommerce-website/src/pages/checkout/checkout.component.jsx
@@ -6,6 +6,7 @@ import './checkout.styles.scss'
 import CartItem from "../../components/cart-item/cart-item.component";
 import { selectCartTotal } from "../../redux/cart/cart.selectors";
 import CheckOutItem from "../../components/checkout-item/checkout-item.component";
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
 
 const CheckOut = ({ cartItems, total }) => (
     <div className='checkout-page'>
@@ -32,6 +33,12 @@ const CheckOut = ({ cartItems, total }) => (
             ))
         }
         <div className='total'>TOTAL: ${total}</div>
+        <div className='test-warning'>
+            *Please use the following test credit card for payments*
+            <br />
+            4242 4242 4242 4242 - Exp: 01/28 - CVV: 123
+        </div>
+        <StripeCheckoutButton price={ total }/>
     </div>
 )
 
