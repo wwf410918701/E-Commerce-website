b7deebbec8e902e38c70968f80310c30d0c80bd0
WeiFeng Wu <wwf410918701@126.com>
2021-12-02 15:06:45 +0800

    using react hook useState to trun shopPage component from class to functional component

--- ecommerce-website/src/pages/shop/shop.component.jsx
+++ ecommerce-website/src/pages/shop/shop.component.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component,useEffect } from "react";
 import { Route } from 'react-router-dom';
 import ContainerCollectionPage from "../../components/collection/collection.container";
 import { connect } from "react-redux";
@@ -8,32 +8,21 @@ import { selectIsCollectionsLoaded } from "../../redux/shop/shop.selectors";
 import CollectionOverviewContainer from "../../components/collection-overview/collection-overview.container";
 
 
-class ShopPage extends React.Component {
-    componentDidMount() {
-        const { fetchCollectionStart } = this.props
-
+const ShopPage = ({ fetchCollectionStart, match }) => {
+    useEffect(() => {
         fetchCollectionStart();
-    }
+    }, [fetchCollectionStart])
 
-
-    render() {
-        const { match, isLoaded } = this.props;
-
-        return (
-            <div className='shop-page'>
-                <Route exact path={`${match.path}`} component={CollectionOverviewContainer} />
-                <Route path={`${match.path}/:collectionId`} component={ContainerCollectionPage} />
-            </div>
-        )  
-    }
+    return (
+        <div className='shop-page'>
+            <Route exact path={`${match.path}`} component={CollectionOverviewContainer} />
+            <Route path={`${match.path}/:collectionId`} component={ContainerCollectionPage} />
+        </div>
+    )  
 }
 
 const mapDispatchToProps = dispatch => ({
     fetchCollectionStart: () => dispatch(fetchCollectionStart())
 })
-  
-const mapStateToProps = createStructuredSelector({
-    isLoaded: selectIsCollectionsLoaded
-})
 
-export default connect(mapStateToProps, mapDispatchToProps)(ShopPage);
+export default connect(null, mapDispatchToProps)(ShopPage);
