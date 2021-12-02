f7176b4680ab4542bd93961894b23e922177c0f8
WeiFeng Wu <wwf410918701@126.com>
2021-12-02 15:02:49 +0800

    using react hook useState to trun app component from class to functional component, fix bugs at that not automatically redirects to homepage if user access signin page when they already signed in

--- ecommerce-website/src/App.js
+++ ecommerce-website/src/App.js
@@ -4,34 +4,32 @@ import ShopPage from './pages/shop/shop.component'
 import {Route, Switch, Redirect} from 'react-router-dom'
 import Header from './components/header/header.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import React from 'react';
+import React, { useEffect} from 'react';
 import { connect } from 'react-redux';
 import { checkUserSession } from './redux/user/user.actions';
 import CheckOut from './pages/checkout/checkout.component';
+import { selectCurrentUser } from './redux/user/user.selectors';
+import { createStructuredSelector } from 'reselect';
 
 
 
-class App extends React.Component {
-  unsubscribeFromAuth = null
+const App = ({ checkUserSession, currentUser }) => {
 
-  componentDidMount() {
-    const { checkUserSession } = this.props
-    
+  useEffect(() => {
     checkUserSession()
-  }
+  }, [checkUserSession]) 
 
-  render() {
-    return (
-      <div>
-        <Header/>
-        <Switch>
-          <Route path='/shop' component={ShopPage}/>
-          <Route exact path='/signin' render={() => (this.props.currentUser? <Redirect to='/' /> : <SignInAndSignUp />)}/>
-          <Route exact path='/checkout' component={CheckOut}></Route>
-          <Route exact path='/' component={HomePage}/>
-        </Switch>
-      </div>
-  );}
+  return (
+    <div>
+      <Header/>
+      <Switch>
+        <Route path='/shop' component={ShopPage}/>
+        <Route exact path='/signin' render={() => (currentUser? <Redirect to='/' /> : <SignInAndSignUp />)}/>
+        <Route exact path='/checkout' component={CheckOut}></Route>
+        <Route exact path='/' component={HomePage}/>
+      </Switch>
+    </div>
+  );
 }
 
 
@@ -39,4 +37,8 @@ const mapDispatchToProps = dispatch => ({
   checkUserSession: user => dispatch(checkUserSession())
 });
 
-export default connect(null, mapDispatchToProps)(App);
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
