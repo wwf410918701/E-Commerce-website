c42f74ac4e72f7a30785accb8b03363e823dda7e
WeiFeng Wu <wwf410918701@126.com>
2021-12-05 22:36:26 +0800

    fix bugs in the diretory of server

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'client/build', 'index.html')))
+    app.use(express.static(path.join(__dirname, 'ecommerce-website/build', 'index.html')))
     app.get('*', function(req, res) {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+        res.sendFile(path.join(__dirname, 'ecommerce-website/build', 'index.html'))
     })
 }
 
